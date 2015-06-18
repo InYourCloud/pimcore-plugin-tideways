679081e7d5124f1ed83e1ff4d23219e0af5ff829
Christoph Luehr <christoph.luehr@basilicom.de>
2015-06-18 15:30:06 +0200

    cleanup, rewrite, added more options

---
+++ README.md
@@ -0,0 +1,75 @@
+Tideways Pimcore Plugin
+================================================
+
+Developer info: [Pimcore at basilicom](http://basilicom.de/en/pimcore)
+
+## Synopsis
+
+This Pimcore http://www.pimcore.org plugin simplifies using
+and configuring the Tideways Profiler.
+
+## Code Example / Method of Operation
+
+If installed and enabled, the following Tideways properties
+are configured:
+
+* \Tideways\Profiler::detectFramework(\Tideways\Profiler::FRAMEWORK_ZEND_FRAMEWORK1);
+
+## Motivation
+
+Tideways works "out of the box" with ZF1 Framework detection.
+In order to configure certain aspects - mainly custom span timeline
+events - additional code is needed. This plugin simplifies this
+process by adding a layer in between the Tideways API calls and
+an easy XML based configuration file accessable via the Pimcore
+Plugin management system.
+
+## Installation
+
+Install the Tideways system on your server, see: https://tideways.io/profiler/docs/getting-started/installation
+
+Make sure you disable the Tideways auto-start in your php.ini: tideways.auto_start=No (The plugin takes care of that.)
+
+Add "basilicom-pimcore-plugin/tideways" as a requirement to the
+composer.json in the toplevel directory of your Pimcore installation.
+
+Example:
+
+    {
+        "require": {
+            "basilicom-pimcore-plugin/tideways": ">=1.0.0"
+        }
+    }
+    
+Install the plugin via the Pimcore Extension Manager. Press the "Configure" button of the
+Tideways plugin from within the Extension Manager and set the "apiKey" property accordingly.
+
+## API Reference
+
+The following static methods are provided as a wrapper for the original
+Tideways functions:
+ 
+* \Tideways\Plugin::setTransactionName(string $transactionName)
+* \Tideways\Plugin::setCustomVariable(string $variable, mixed $value)
+
+Not implemented, yet:
+
+* \Tideways\Plugin::createEvent(string $eventName, string $environment='production', string $type='release')
+
+## Tests
+
+* none
+
+## Todo
+
+* Implement exception handler tracing 
+* Implement event creation
+
+## Contributors
+
+* Tim Jagodzinski <tim.jagodzinski@basilicom.de>
+* Christoph Luehr <christoph.luehr@basilicom.de>
+
+## License
+
+* BSD-3-Clause
--- lib/Tideways/Plugin.php
+++ lib/Tideways/Plugin.php
@@ -1,54 +1,197 @@
 <?php
+/**
+ * Tideways Pimcore Plugin
+ */
 
 namespace Tideways;
 
 use Pimcore\API\Plugin as PluginLib;
 
+/**
+ * Class Plugin
+ *
+ * @package Tideways
+ */
 class Plugin extends PluginLib\AbstractPlugin implements PluginLib\PluginInterface {
 
-    public function init() {
-        if (class_exists('Tideways\Profiler') && self::isInstalled()) {
-            self:: initTidewaysWatchers();
+    const SAMPLE_CONFIG_XML = "/Tideways/tideways.xml";
+    const CONFIG_XML = '/var/config/tideways.xml';
+
+    /**
+     * @var bool store enabled state - set in config xml
+     */
+    private static $isEnabled = false;
+
+    /**
+     * Initialize Plugin
+     *
+     * Sets up Tideways, watchers, apiKey, various config options
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (!@class_exists('Tideways\Profiler')) {
+            return;
         }
+
+        if (!self::isInstalled()) {
+            return;
+        }
+
+        $config = new \Zend_Config_Xml(self::getConfigName());
+
+        self::$isEnabled = ($config->tideways->get('enabled', '0') == '1');
+        if (!self::$isEnabled) {
+            return;
+        }
+
+        $apiKey = $config->tideways->get('apiKey', '');
+        if ($apiKey == '') {
+            return;
+        }
+
+        \Tideways\Profiler::detectFramework(\Tideways\Profiler::FRAMEWORK_ZEND_FRAMEWORK1);
+
+        $sampleRate = (int)$config->tideways->get('sampleRate', 10);
+
+        \Tideways\Profiler::start($apiKey, $sampleRate); // 20% Sample-Rate
+
+        foreach ($config->tideways->watchers->watcher as $watcher) {
+            \Tideways\Profiler::watch($watcher);
+        }
+
+        self::$isEnabled = true;
+
+        // @todo add exception handler watcher:
+        // \Tideways\Profiler::detectExceptionFunction('my_exception_handler');
+        //\Tideways\Profiler::detectExceptionFunction('\Whoops\Run::handleException');
+
     }
 
-    public static function initTidewaysWatchers(){
-        $configXml = new \DOMDocument();
-        $configXml->load(PIMCORE_PLUGINS_PATH."/Tideways/config.xml");
-        $additionalWatchers = $configXml->getElementsByTagName('additionalWatcher');
+    /**
+     * Install plugin
+     *
+     * Copies sample XML to website config path if it does not exist yet.
+     * Sets config file parameter "installed" to 1 and "enabled" to "1"
+     *
+     * @return string install success|failure message
+     */
+    public static function install()
+    {
+        if (!file_exists(self::getConfigName())) {
 
-        foreach($additionalWatchers as $additionalWatcher){
-            tideways_span_watch($additionalWatcher->nodeValue);
+            $defaultConfig = new \Zend_Config_Xml(PIMCORE_PLUGINS_PATH . self::SAMPLE_CONFIG_XML);
+            $configWriter = new \Zend_Config_Writer_Xml();
+            $configWriter->setConfig($defaultConfig);
+            $configWriter->write(self::getConfigName());
         }
-    }
 
-	public static function install (){
-        if(!is_dir(self::getInstallCheckPath())) {
-            mkdir(self::getInstallCheckPath()); //use folder as 'installed' marker
-        }
+        $config = new \Zend_Config_Xml(self::getConfigName(), null, array('allowModifications' => true));
+        $config->tideways->installed = 1;
+        $config->tideways->enabled = 1;
+
+        $configWriter = new \Zend_Config_Writer_Xml();
+        $configWriter->setConfig($config);
+        $configWriter->write(self::getConfigName());
 
         if (self::isInstalled()) {
             return "Successfully installed.";
         } else {
             return "Could not be installed";
         }
-	}
-	
-	public static function uninstall (){
-        rmdir(self::getInstallCheckPath()); //use folder as 'installed' marker
+    }
+
+    /**
+     * Uninstall plugin
+     *
+     * Sets config file parameter "installed" to 0 (if config file exists)
+     *
+     * @return string uninstall success|failure message
+     */
+    public static function uninstall()
+    {
+        if (file_exists(self::getConfigName())) {
+
+            $config = new \Zend_Config_Xml(self::getConfigName(), null, array('allowModifications' => true));
+            $config->tideways->installed = 0;
+
+            $configWriter = new \Zend_Config_Writer_Xml();
+            $configWriter->setConfig($config);
+            $configWriter->write(self::getConfigName());
+        }
 
         if (!self::isInstalled()) {
             return "Successfully uninstalled.";
         } else {
             return "Could not be uninstalled";
         }
-	}
-
-    public static function isInstalled () {
-        return is_dir(self::getInstallCheckPath());
     }
 
-    public static function getInstallCheckPath() {
-        return PIMCORE_PLUGINS_PATH."/Tideways/installed";
+    /**
+     * Determine plugin install state
+     *
+     * @return bool true if plugin is installed (option "installed" is "1" in config file)
+     */
+    public static function isInstalled()
+    {
+        if (!file_exists(self::getConfigName())) {
+            return false;
+        }
+
+        $config = new \Zend_Config_Xml(self::getConfigName());
+        if ($config->tideways->installed != 1) {
+            return false;
+        }
+        return true;
     }
+
+    /**
+     * Set custom variable for Tideways (only if installed & enabled)
+     *
+     * @param $var
+     * @param $value
+     */
+    public static function setCustomVariable($var, $value)
+    {
+        if (self::$isEnabled) {
+            \Tideways\Profiler::setCustomVariable($var, $value);
+        }
+    }
+
+    /**
+     * Set custom transaction name for Tideways (only if installed & enabled)
+     *
+     * @param $transactionName
+     */
+    public static function setTransactionName($transactionName)
+    {
+        if (self::$isEnabled) {
+            \Tideways\Profiler::setTransactionName($transactionName);
+        }
+    }
+
+    /**
+     * Create Tideways event annotation via API
+     *
+     * @param $title
+     * @param string $environment
+     * @param string $type
+     * @todo implement event creation
+     */
+    public static function createEvent($title, $environment='production', $type='release')
+    {
+        //curl -d '{"apiKey": "api key here", "name": "v1.0 released", "environment": "production", "type": "release"}' -X POST https://app.tideways.io/api/events
+    }
+
+    /**
+     * Return config file name
+     *
+     * @return string xml config filename
+     */
+    private static function getConfigName()
+    {
+        return PIMCORE_WEBSITE_PATH . self::CONFIG_XML;
+    }
+
 }
